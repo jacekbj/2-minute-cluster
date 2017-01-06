556242aa6339b5181dfa8a7fd7373f7e975c855b
Jacek Jankowski <jacek.j.jankowski@gmail.com>
2017-01-06 16:52:59 +0100

    Fabric working fully.

--- fabfile.py
+++ fabfile.py
@@ -6,9 +6,6 @@ import json
 from fabric.api import task, require, local, env
 
 
-DEFAULT_CLUSTER_NAME = '2-minute-cluster'
-
-
 @task
 def build_image():
     local('docker build -t spark_standalone:latest .')
@@ -26,25 +23,32 @@ def read_config(path):
         config = json.loads(f.read())
         env['data_files_dir'] = config['data_files_dir']
         env['source_files_dir'] = config['source_files_dir']
+        env['storage_name'] = config['storage_name']
+        env['cluster_name'] = config['cluster_name']
 
 
 @task
-def create_cluster(cluster_name):
+def create_cluster(workers_count=4):
     """
     Runs gcloud command to create a cluster.
 
     :param str cluster_name: name of cluster to create
     :return: None
     """
+    require('cluster_name')
     command = (
         'gcloud dataproc clusters create'
         ' {}'
-        ' --num-workers 4'
+        ' --num-workers {}'
+        ' --master-boot-disk-size 10GB'
+        ' --worker-boot-disk-size 10GB'
+        ' --preemptible-worker-boot-disk-size 10GB'
         ' --master-machine-type n1-standard-1'
         ' --worker-machine-type n1-standard-1'
         ' --num-master-local-ssds 0'
         ' --num-worker-local-ssds 0'
-        ' --zone europe-west1-b'.format(cluster_name)
+        ' --zone europe-west1-b'.format(
+            workers_count, env.cluster_name)
     )
     local(command)
 
@@ -61,7 +65,7 @@ def delete_cluster(cluster_name):
 
 
 @task
-def upload_data_files(storage_name):
+def upload_data_files():
     """
     Runs gcloud to upload data files from env.data_files_dir
     to cloud storage.
@@ -70,13 +74,16 @@ def upload_data_files(storage_name):
     :return: None
     """
     require('data_files_dir')
-    local('gsutil -m cp -r {} gs://{}/data'.format(
-        env.data_files_dir, storage_name)
+    require('storage_name')
+    local(
+        'docker run --rm -ti --volumes-from gcloud-config -v {}:/tmp/data google/cloud-sdk'
+        ' gsutil -m cp -r /tmp/data gs://{}/data'.format(
+            env.data_files_dir, env.storage_name)
     )
 
 
 @task
-def upload_source_files(storage_name):
+def upload_source_files():
     """
     Runs gcloud to upload source files from env.data_files_dir
     to cloud storage.
@@ -85,19 +92,12 @@ def upload_source_files(storage_name):
     :return: None
     """
     require('source_files_dir')
-    local('gsutil -m cp -r {} gs://{}/src'.format(
-        env.source_files_dir, storage_name)
+    require('storage_name')
+    local(
+        'docker run --rm -ti --volumes-from gcloud-config -v {}:/tmp/src google/cloud-sdk'
+        ' gsutil -m cp -r /tmp/src gs://{}'.format(
+            env.source_files_dir, env.storage_name)
     )
-
-
-def run_log_analysis(cluster_name):
-    """
-    Run gcloud to submit task via pysubmit.
-
-    :type str cluster_name: name of cluster to run the task on
-    :return:
-    """
-    raise NotImplementedError
 
 
 @task
@@ -110,10 +110,23 @@ def run_locally():
 
 
 @task
-def create_cluster(cluster_name=DEFAULT_CLUSTER_NAME, workers='8'):
+def run_remote():
+    local(
+        'docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk'
+        ' gcloud dataproc jobs submit pyspark --cluster {cluster_name}'
+        ' gs://{storage_name}/src/main.py'
+        ' gcloud -s {storage_name}'.format(
+            cluster_name=env.cluster_name,
+            storage_name=env.storage_name)
+    )
+
+
+@task
+def create_cluster(workers='4'):
     command_parts = (
-        'gcloud dataproc clusters create',
-        '{}'.format(cluster_name),
+        'docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk'
+        ' gcloud dataproc clusters create',
+        ' {}'.format(env.cluster_name),
         '--num-preemptible-workers 0',
         '--num-workers {}'.format(workers),
         '--master-machine-type n1-highmem-2',
@@ -127,5 +140,8 @@ def create_cluster(cluster_name=DEFAULT_CLUSTER_NAME, workers='8'):
 
 
 @task
-def delete_cluster(cluster_name=DEFAULT_CLUSTER_NAME):
-    local('gcloud --quiet dataproc clusters delete {}'.format(cluster_name))
+def delete_cluster():
+    local(
+        'docker run --rm -ti --volumes-from gcloud-config google/cloud-sdk'
+        ' gcloud --quiet dataproc clusters delete {}'.format(env.cluster_name)
+    )
